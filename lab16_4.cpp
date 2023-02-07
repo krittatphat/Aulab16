d7ccafcd757172be1763682f85ff0bcc6d039050
Krittatphat <kittadpast.402@hotmail.com>
2023-02-07 16:11:01 +0700

    Completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,21 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a, int &b, int &c, int &d){
+	int x[4] ;
+	int y[] = {a, b ,c ,d} ;
+	for(int i=0 ; i < 4 ; i++){
+		x[i] = rand()%4 ;
+		for(int j = 0 ; j < i ; j++){
+			if(x[i]== x[j]){
+				i-- ;
+				break ;
+			}
+		}
+	}
+	a = y[x[0]] ; 
+	b = y[x[1]] ; 
+	c = y[x[2]] ; 
+	d = y[x[3]] ; 
+}
